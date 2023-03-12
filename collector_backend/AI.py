f25bfee09266fa38b99ebd53b9b74d2a891122f1
0marSater <111258496+0marSater@users.noreply.github.com>
2023-03-12 21:16:59 +0200

    first commit

---
+++ collector_backend/AI.py
@@ -0,0 +1,162 @@
+import csv
+import os
+import cv2
+import mediapipe as mp
+import numpy as np
+
+
+# creation of csv file for the landmarks cords
+def create_landmarks_cords():
+    num_cords = 543
+    landmarks = ['action']
+    for val in range(1, num_cords + 1):
+        landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
+        with open('pose_images.csv', mode='w', newline='') as f:
+            csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(landmarks)
+
+
+# put filename to be string and action name to be string too
+def save_landmarks(filename, action_name, pose, face, hands):
+    # putting the solutions into small variables to call them
+    mp_drawing = mp.solutions.drawing_utils
+    mp_holistic = mp.solutions.holistic
+
+    # capturing the video applicable to change in the future
+    cap = cv2.VideoCapture(filename)
+
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Recolor Feed
+            image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+
+            # make image not writeable for extra performance that doesn't exist ?
+            image.flags.writeable = False
+
+            # Make Detections
+            results = holistic.process(image)
+            if not os.path.isfile('pose_images.csv'):
+                create_landmarks_cords()
+
+            # make image writeable again
+            image.flags.writeable = True
+
+            # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
+
+            # Recolor image back to BGR for rendering
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # 1. Draw face landmarks
+            mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                                      mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                                      mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                                      )
+
+            # 2. Right hand
+            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                                      )
+
+            # 3. Left Hand
+            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                                      )
+
+            # 4. Pose Detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                      )
+            # Export to CSV
+            with open('pose_images.csv', mode='a', newline='') as f:
+                csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(extract_landmarks(results, action_name, pose, face, hands))
+
+            cv2.imshow('video', image)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# extract the landmarks and put zero if didn't detect the landmark
+def extract_landmarks(results, action_name, pose, face, hands):
+    if pose and not face and not hands:
+        savedpose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                              results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(
+            33 * 4)
+        savedface = np.zeros(468 * 4)
+        lh = np.zeros(21 * 4)
+        rh = np.zeros(21 * 4)
+    elif not pose and face and not hands:
+        savedpose = np.zeros(33 * 4)
+        savedface = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                              results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(
+            468 * 4)
+        lh = np.zeros(21 * 4)
+        rh = np.zeros(21 * 4)
+    elif not pose and not face and hands:
+        savedpose = np.zeros(33 * 4)
+        savedface = np.zeros(468 * 4)
+        lh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                       results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(
+            21 * 4)
+        rh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                       results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+            21 * 4)
+    elif pose and face and not hands:
+        savedpose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                              results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(
+            33 * 4)
+        savedface = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                              results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(
+            468 * 4)
+        lh = np.zeros(21 * 4)
+        rh = np.zeros(21 * 4)
+    elif pose and not face and hands:
+        savedpose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                              results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(
+            33 * 4)
+        savedface = np.zeros(468 * 4)
+        lh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                       results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(
+            21 * 4)
+        rh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                       results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+            21 * 4)
+    elif not pose and face and hands:
+        savedpose = np.zeros(33 * 4)
+        savedface = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                              results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(
+            468 * 4)
+        lh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                       results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(
+            21 * 4)
+        rh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                       results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+            21 * 4)
+    elif pose and face and hands:
+        savedpose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                              results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(
+            33 * 4)
+        savedface = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                              results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(
+            468 * 4)
+        lh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                       results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(
+            21 * 4)
+        rh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                       results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+            21 * 4)
+    else:
+        print("No boolean values set to True")
+    row = list(np.concatenate([savedpose, savedface, lh, rh]))
+    # Append action name
+    row.insert(0, action_name)
+    return row
---
+++ collector_backend/main.py
@@ -0,0 +1,96 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet, configure_uploads
+from AI import *
+
+app = Flask(__name__)
+cors = CORS(app)
+
+# take the path of the project
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'dataBase.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOADED_VIDEOS_DEST'] = 'videos\\'
+app.config['VIDEO_WITH_LANDMARKS'] = 'VideWithLandMarks\\'
+
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
+
+dataBase = SQLAlchemy(app)
+
+videos = UploadSet('videos', extensions=('mp4', 'mov', 'avi'))
+
+# Configure the UploadSet
+
+configure_uploads(app, videos)
+
+
+class Video_data(dataBase.Model):
+    id = dataBase.Column(dataBase.Integer, primary_key=True)
+    action = dataBase.Column(dataBase.String(100), nullable=False)
+    context = dataBase.Column(dataBase.String(255), nullable=False)
+    meaning = dataBase.Column(dataBase.String(255), nullable=True)
+    pose = dataBase.Column(dataBase.Boolean, nullable=True)
+    face = dataBase.Column(dataBase.Boolean, nullable=True)
+    hand = dataBase.Column(dataBase.Boolean, nullable=True)
+    video = dataBase.Column(dataBase.String(255), nullable=False)
+
+
+with app.app_context():
+    dataBase.create_all()
+    # dataBase.drop_all()
+
+
+@app.route('/display-data', methods=['GET'])
+def get_data():
+    data = Video_data.query.all()
+    allRows = []
+    for row in data:
+        allRows.append({
+            'id': row.id,
+            'action': row.action,
+            'context': row.context,
+            'meaning': row.meaning,
+            'video': row.video
+        })
+    return jsonify({
+        'Data': {
+            'response_data': allRows
+        }
+    })
+
+
+@app.route('/upload-data', methods=['POST'])
+def getVideo():
+    _video_file = request.files['video']
+    _action = request.form['action']
+    pose = get_pose(_action)
+    face = get_face(_action)
+    hand = get_hand(_action)
+    file_name = videos.save(_video_file)
+    full_path = basedir + '\\' + app.config['UPLOADED_VIDEOS_DEST'] + file_name
+    # full_path_2 = basedir + '\\' + app.config['VIDEO_WITH_LANDMARKS']
+    save_landmarks(full_path, _action, pose, face, hand)
+    return "ok"
+
+
+def get_pose(_action):
+    pose_tuple = Video_data.query.filter_by(action=_action).with_entities(Video_data.pose).first()
+    pose_bool = pose_tuple[0]
+    return pose_bool
+
+
+def get_face(_action):
+    face_tuple = Video_data.query.filter_by(action=_action).with_entities(Video_data.face).first()
+    face_bool = face_tuple[0]
+    return face_bool
+
+
+def get_hand(_action):
+    hand_tuple = Video_data.query.filter_by(action=_action).with_entities(Video_data.hand).first()
+    hand_bool = hand_tuple[0]
+    return hand_bool
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
