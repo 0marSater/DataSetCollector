35427a072ec7e95f476d55a4fe3be9bcb135cd3e
0marSater <omar.buis22@gmail.com>
2023-05-17 18:43:29 +0300

    update push.groovy

--- push.groovy
+++ push.groovy
@@ -7,11 +7,11 @@ def pushImage(){
 
     echo "Start pushing frontend image $FRONTEND_IMAGE_TAG ..."
     sh "cd ${WORKSPACE}/collector_frontend && docker tag $FRONTEND_IMAGE_TAG $DOCKER_REPO:$FRONTEND_IMAGE_TAG"
-    sh "docker push $DOCKER_REPO:$FRONTEND_IMAGE_TAG"
+    // sh "docker push $DOCKER_REPO:$FRONTEND_IMAGE_TAG"
     
     echo "Start pushing frontend image $BACKEND_IMAGE_TAG ..."
     sh "cd ${WORKSPACE}/collector_backend && docker tag $BACKEND_IMAGE_TAG $DOCKER_REPO:$BACKEND_IMAGE_TAG"
-    sh "docker push $DOCKER_REPO:$BACKEND_IMAGE_TAG"
+    // sh "docker push $DOCKER_REPO:$BACKEND_IMAGE_TAG"
 
 }
 
